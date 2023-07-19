549efe9144de608bd84c09056ac205dd4ac5e0ab
shynekomaid <34548743+shynekomaid@users.noreply.github.com>
2023-07-19 10:46:33 +0300

    Changes to pass lint

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from telethon import TelegramClient, events, errors
 from telethon.sessions import StringSession
 
 def load_config(file_path):
+    """
+    Load JSON data from a file and return the parsed data.
+
+    Args:
+        file_path (str): The path to the JSON file.
+
+    Returns:
+        dict: The parsed JSON data as a dictionary.
+    """
     with open(file_path) as file:
         return json.load(file)
 
+# Load config and language data
 config = load_config('config.json')
 lang = load_config('lang.json')
 
@@ -26,16 +36,22 @@ CHANNEL_ID = config['CHANNEL_ID']
 
 @client.on(events.NewMessage)
 async def auto_comment(event):
+    """
+    Automatically post a random comment in response to new messages in the configured channel.
+
+    Args:
+        event (telethon.tl.custom.Message): The new message event.
+    """
     if event.chat_id not in CHANNEL_ID:
         return
     print(lang['NEW_POST'].format(event.peer_id.channel_id))
     try:
         await client.send_message(event.chat_id, random.choice(COMMENT_TEXT), comment_to=event.id)
         print(lang['COMMENTED'].format(event.peer_id.channel_id))
-    except errors.FloodWaitError as e:
-        print(lang['FLOOD_WAIT_ERROR'].format(e.seconds))
-        time.sleep(e.seconds)
-    except Exception as e:
+    except errors.FloodWaitError as flood_wait_duration:
+        print(lang['FLOOD_WAIT_ERROR'].format(flood_wait_duration.seconds))
+        time.sleep(flood_wait_duration.seconds)
+    except Exception as unknown_error:
         print(lang['ERROR_WHILE_POSTING'] + '\n' + str(e))
 
 print(lang['STARTED_MSG'])
